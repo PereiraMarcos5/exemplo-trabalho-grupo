da8357c1e78914b584d4760487e227f0083eb2b5
Leonardo Forbici <38699633+LeonardoForbici@users.noreply.github.com>
2018-05-03 16:42:56 -0300

    Fazendo cadastro de livro

---
+++ NossaAplicação/NossaAplicação/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NossaAplicação
+{
+    class Livro
+    {
+        string[] LivroNome = new string[100];
+        string[] genero = new string[100];
+        double[] precolivro = new double[100];
+        int atual = 0;
+
+        public void CadastrarLivro()
+        {
+            Console.Clear();
+            Console.Write("Nome do livro: ");
+            LivroNome[atual] = Console.ReadLine();
+            Console.Write("Genero do livro: ");
+            
+            Console.Write("Preço do barco: ");
+
+        }
+    }
+}
