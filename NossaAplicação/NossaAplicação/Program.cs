66d7e4804d2e6a42ccca68648ba82b07a72e4aec
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-05-03 14:22:44 -0300

    Mudança no meu Cw

--- NossaAplicação/NossaAplicação/Program.cs
+++ NossaAplicação/NossaAplicação/Program.cs
@@ -10,7 +10,7 @@ namespace NossaAplicação
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("EU \"Marcos Pereira\" QUERO IMPRIMIR");
+            Console.WriteLine("Eu \"Marcos Pereira\" mandei imprimir");
             Console.WriteLine("Eu \" Leonardo Forbici\" mandei imprimir");
 
 
