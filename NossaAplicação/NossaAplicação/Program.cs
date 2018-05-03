7de0a1a09378b5277cea099d8404a0f1a781a93f
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-05-03 14:08:37 -0300

    Incialização do repositório com mensagem

---
+++ NossaAplicação/NossaAplicação/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NossaAplicação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EU \"Marcos Pereira\" QUERO IMPRIMIR");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
